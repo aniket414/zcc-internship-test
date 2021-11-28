dfa5d1494d1d79ecd0978b10d486631780f55f27
Aniket Pandey <aniket414@gmail.com>
2021-11-27 23:42:56 -0500

    removed unwanted code

--- Zendesk.Ticket.Viewer/Zendesk.Ticket.Viewer.Service/TicketService.cs
+++ Zendesk.Ticket.Viewer/Zendesk.Ticket.Viewer.Service/TicketService.cs
@@ -11,32 +11,7 @@ namespace Zendesk.Ticket.Viewer.Service
 
         public async Task<PagedList<Ticket>> GetAllTicketsAsync(string applicationId, int pageNumber, int pageSize)
         {
-            var isExists = await _applicationService.ExistsAsync(applicationId);
-            if (!isExists)
-                throw Errors.ClientSide.EntityDoesNotExist("application", "ID", applicationId);
-
-            var tenantId = TenantHelper.GetTenantForRoles(out var accountId);
-            var store = await _storeFactory.BuildRoleStoreAsync(tenantId);
-
-            var roles = await store.GetAllAsync(applicationId, pageSize: _defaultPageSize);
-            if (roles == null || roles.Count == 0)
-                return null;
-
-            var totalRecords = roles.Count;
-
-            var sortedRoles = SortRoles(roles);
-
-            var pagedRoles = GetRolesAccordingToPaging(sortedRoles, pageNumber, pageSize);
-            pagedRoles.TotalRecords = totalRecords;
-
-            foreach (var pagedRole in pagedRoles)
-            {
-                if (!string.IsNullOrWhiteSpace(pagedRole.Description))
-                    pagedRole.Description = pagedRole.Description.JavascriptEncode().HtmlEncode();
-            }
-
-
-            return pagedRoles;
+            
         }
     }
 }

2382cf16841a2bedfa0e6c7dbb071285d3f16afc
Aniket Pandey <aniket414@gmail.com>
2021-11-29 01:56:26 -0500

    added translator tests

---
+++ Zendesk.Ticket.Viewer/Zendesk.Ticket.Viewer.UnitTests/TicketDataContractTranslatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using Zendesk.Ticket.Viewer.Service;
+
+namespace Zendesk.Ticket.Viewer.UnitTests
+{
+    public class TicketDataContractTranslatorTests
+    {
+        [Fact]
+        public void Given_TicketTranslator_When_ValidTicketResponse_Then_ShouldTranslateToModelSuccessfully()
+        {
+            //build
+            var ticket = ObjectBuilder.TicketBuilder()
+                .SetURL("test.com")
+                .SetId(1)
+                .Build();
+
+            //act 
+            var response = ticket.ToGetTicketsDataContract();
+
+            //assert
+            response.Should().NotBeNull();
+            response.URL.Should().Be(ticket.URL);
+            response.Id.Should().Be(ticket.Id);
+        }
+
+        [Fact]
+        public void Given_TicketTranslator_When_TicketResponseIsNull_Then_ShouldTranslateToModelSuccessfully_And_ReturnNull()
+        {
+            //build
+            Domain.Ticket ticket = null;
+
+            //act 
+            var response = ticket.ToGetTicketsDataContract();
+
+            //assert
+            response.Should().BeNull();
+        }
+    }
+}
